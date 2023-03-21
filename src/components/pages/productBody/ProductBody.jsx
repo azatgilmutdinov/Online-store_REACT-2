2db236255fee720802b7ec2cab1d10790b570bce
Azat G <azot_777i@inbox.ru>
2023-03-21 19:29:09 +0300

    modern btn, product page, scroll, array products

--- src/components/pages/productBody/ProductBody.jsx
+++ src/components/pages/productBody/ProductBody.jsx
@@ -10,7 +10,7 @@ import { useSelector } from 'react-redux'
 const ProductBody = () => {
   const { id } = useParams();
   console.log('id: ', id);
-  const {urlImg, title, descr, price, weight, unit } = useSelector(state => state.productsList.productsList.find(item => item.id == id))
+  const {urlImg, title, body, price, weight, unit } = useSelector(state => state.productsList.productsList.find(item => item.id == id))
 
   console.log('urlImg', urlImg);
   
@@ -42,7 +42,7 @@ const ProductBody = () => {
           />
           <div className={`${styles['productBody__main-cardBody']} ${styles['main-cardBody']}`}>
             <h1 className={styles['main-cardBody__title']}>{title}</h1>
-            <div className={styles['main-cardBody__descr']}>{descr}</div>
+            <div className={styles['main-cardBody__descr']}>{body}</div>
             <div className={styles['main-cardBody__price-weight-btnBasket']}>
               <div className={styles["price-weight-btnBasket"]}>
                 <span className={styles['price-weight-btnBasket__price']}>{Intl.NumberFormat("ru").format(price)} ₽ / </span>
