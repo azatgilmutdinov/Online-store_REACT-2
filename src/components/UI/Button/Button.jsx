84d9db0cf8a50e134f55d5959f3b3419853c7498
Azat G <azot_777i@inbox.ru>
2023-03-22 21:32:22 +0300

    add custom checkbox

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -4,7 +4,11 @@ import styles from './Button.module.css'
 const Button = ({text, type, onclickFunction, disabled}) => {
   
   return (
-    <button disabled={disabled} className={type==='orange' ?  `${styles.button} ${styles.buttonOrange}` : `${styles.button} ${styles.buttonLimpid}`} onClick={onclickFunction}>{text}</button>
+    <button 
+      disabled={disabled} 
+      className={type==='orange' ?  `${styles.button} ${styles.buttonOrange}` : `${styles.button} ${styles.buttonLimpid}`} 
+      onClick={onclickFunction}
+    >{text}</button>
   )
 }
 
--- src/components/blocks/FormReg/Form.jsx
+++ src/components/blocks/FormReg/Form.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Button from '../../UI/Button/Button'
 import './Form.css'
 
 const Form = ({title, linkText, link, buttonText}) => {
-  return (
+
+  const [valueMess, setValuMess] = useState('')
+
+  const testFunc = (event) => {
+    setValuMess(event.target.value)
+    console.log('event.target.value', event.target.value);
+    // console.log('valueMess', valueMess);
+  }
+  if (valueMess.length < 5) {
+
+  }
+
+  return (    
     <form action="#" className="form">
       <Link to={link} className="form__link">{linkText}</Link>
       <h2 className="form__title">{title}</h2>
       <div className="form__wrapperInput form__wrapperInput_email">
-        <input type="email" className="form__input form-input form-input_email" placeholder="Логин"></input>
+        <input 
+          type="email" 
+          className="form__input form-input form-input_email" 
+          placeholder="Логин"
+          value={valueMess}
+          onInput={testFunc}
+        ></input>
 
-        <p className="form__text-error text-error text-error_email-invalid">Логин должен содержать не менее 4-х символов</p>
+        <p 
+          className="form__text-error text-error text-error_email-invalid"
+        >
+          Логин должен содержать не менее 4-х символов
+        </p>
         <p className="form__text-error text-error text-error_email-empty">Поле не должно быть пустым</p>
       </div>
       
@@ -46,32 +68,3 @@ const Form = ({title, linkText, link, buttonText}) => {
 
 export default Form
 
-{/* <a href="#" class="form-authorization__registration authorization-registration">Зарегистрироваться</a>
-      <h2 class="form-authorization__title">Войти</h2>
-
-      <p class="form__name form-authorization__name-email">Email</p>
-      <div class="form-authorization__input-wrapper form-authorization__input-wrapper_email">
-        <span class="form-authorization__star form-authorization__star-email">*</span>
-        <input type="email" class="form__input form-authorization__input-email" placeholder="Введите email">
-      </div>
-      
-      <p class="form__text-error form-authorization__text-error_email-empty">Поле обязательно для заполнения</p>
-      <p class="form__text-error form-authorization__text-error_email-invalid">Email невалидный</p>
-
-      <p class="form__name form-authorization__name-password">Пароль</p>
-      <div class="form-authorization__input-wrapper form-authorization__input-wrapper_password">
-        <span class="form-authorization__star form-authorization__star-password">*</span>
-        <input type="password" class="form__input form-authorization__input-password" placeholder="Введите пароль">
-      </div>
-      
-      <p class="form__text-error form-authorization__text-error_password-empty">Поле обязательно для заполнения</p>
-      <p class="form__text-error form-authorization__text-error_password-invalid">Email или пароль неверный</p>
-
-      <label  class="form-authorization__input-checkbox authorization-input-checkbox">
-        <span class="form-authorization__star form-authorization__star-checkbox">*</span>
-        <input type="checkbox" class="authorization-input-checkbox__real">
-        <span class="authorization-input-checkbox__custom"></span>
-        <p class="authorization-input-checkbox__text">Я согласен с <a href="#">Правилами пользования приложением</a></p>
-      </label>
-      <p class="form__text-error form-authorization__text-error_checkbox-empty">Поле обязательно для заполнения</p>
-      <button type="submit" class="form-authorization__submit-btn">Вход</button> */}
